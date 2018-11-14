c6957d09925cac103eec1f28434f01c722f272ef
Oscar Lafarga <otech47@gmail.com>
2018-11-14 14:54:56 +0100

    update setup

--- web/components/LandingPage.js
+++ web/components/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from './Button';
+import Card from './Card';
 import Flex from './Flex';
 import Icon from './Icon';
 import Text from './Text';
@@ -9,9 +10,15 @@ export default class LandingPage extends React.Component {
     render() {
         return (
             <div className='LandingPage'>
-                <Flex>
-                    <Text m='1rem'>This is the LandingPage component</Text>
-                    <Button m='1rem'>Click here</Button>
+                <Flex
+                    width='20%'
+                    direction='column'
+                    bg='green'
+                >
+                    <Card>
+                        <Text>12</Text>
+                        <Text>MON</Text>
+                    </Card>
                 </Flex>
             </div>
         );
