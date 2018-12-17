75c23489339c964b99ed656c9d2a131e5cc4ff1a
Oscar Lafarga <otech47@gmail.com>
2018-12-17 08:09:33 -0500

    Merge with setLife/setBlocks to stay up to date and resolve merge conflicts

---
+++ api/handlers/airtable.js
@@ -0,0 +1,60 @@
+const Airtable = require('airtable')
+const {
+    AIRTABLE
+} = require('../config/credentials')
+
+const base = new Airtable({
+    apiKey: AIRTABLE.API_KEY
+}).base(AIRTABLE.BASE_ID)
+
+const airtable = module.exports = (function () {
+
+    const fetchBaseRecords = (params) => {
+        return new Promise((resolve, reject) => {
+            let baseRecords = []
+            
+            base(params.tableName)
+            .select({
+                maxRecords: params.maxRecords || 20,
+                view: params.viewName
+            })
+            .eachPage((records, fetchNextPage) => {
+                records.forEach((record) => {
+                    baseRecords.push(record)
+                });
+
+                fetchNextPage();
+            }, function done(err) {
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                } else {
+                    resolve(baseRecords)
+                }
+            });
+        });
+    };
+
+    const fetchTableRecord = (params) => {
+        return new Promise((resolve, reject) => {
+            base(params.tableName)
+            .find(
+                params.recordId,
+                (err, record) => {
+                    if (err) {
+                        console.error(err);
+                        reject(err);
+                    } else {
+                        resolve(record)
+                    }
+                }
+            );
+        });
+    };
+
+    return {
+        fetchBaseRecords,
+        fetchTableRecord
+    };
+
+})();
--- api/modules/team.js
+++ api/modules/team.js
@@ -1,41 +1,41 @@
 // So many utility functions (https://lodash.com/docs)
 var _ = require('lodash');
 
+
 var team = module.exports = (function() {
+    const airtable = require('../handlers/airtable')
 
     const fetchAllTeamMembers = function (params) {
         return new Promise(function(resolve, reject) {
-            resolve([
-                {
-                    id: '00000001',
-                    name: 'Quinn Pruitt'
-                },
-                {
-                    id: '00000002',
-                    name: 'Oscar Lafarga'
-                },
-                {
-                    id: '00000003',
-                    name: 'David Lafarga'
-                },
-                {
-                    id: '00000004',
-                    name: 'Victoria Lafarga'
-                }
-            ])
+            airtable.fetchBaseRecords({
+                tableName: 'Team',
+                viewName: 'All'
+            })
+            .then(records => {
+                const teamMembers = records.map(r => {
+                    return {
+                        id: r.id,
+                        name: r.fields.Name
+                    }
+                })
+                resolve(teamMembers)
+            })
         });
     };
 
     const fetchTeamMemberById = function (params) {
         return new Promise(function (resolve, reject) {
-            // Fetch from AirTable API handler
             
-            resolve(
-                {
-                    id: '00000000',
-                    name: 'Sample Team Member'
-                }
-            )
+            airtable.fetchTableRecord({
+                tableName: 'Team',
+                recordId: params.id
+            })
+            .then(record => {
+                resolve({
+                    id: record.id,
+                    name: record.fields.Name
+                })
+            })
         });
     };
 
