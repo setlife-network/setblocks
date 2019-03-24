dd22f9bb16c49556c28516e53a42c87f0d5a201d
Oscar Lafarga <otech47@gmail.com>
2019-03-24 18:26:14 +0100

    Add unit test

--- tests/components/TeamMember.test.js
+++ tests/components/TeamMember.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import TeamMember from '../../web/components/TeamMember';
 
+import Text from '../../web/components/Text';
+
 let renderedComponent;
 let MOCK_TEAM_MEMBER = {
     id: '00000001',
@@ -22,6 +24,12 @@ describe('TeamMember Component', () => {
 
     it('should render correctly', () => {
         expect(renderedComponent).toMatchSnapshot();
+    })
+
+    it('should display the correct name', () => {
+        let nameText = renderedComponent.find(Text)
+        
+        expect(nameText.contains(MOCK_TEAM_MEMBER.name)).toBe(true)
     })
 
 });
